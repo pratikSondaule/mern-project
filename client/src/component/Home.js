ff4c07117e15c950242cf6afd1661522ac633709
pratikSondaule <105500816+pratikSondaule@users.noreply.github.com>
2023-04-23 15:12:56 +0530

    update

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom"
+import { useNavigate, NavLink } from "react-router-dom"
 
 
 const Home = () => {
